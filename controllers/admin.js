b522ddad2cfdd515a4dbcece355819823f5211ed
Lorena Criado <criado.manzaneque@gmail.com>
2024-09-03 12:45:39 +0200

    Edit apartment functionality is working

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,15 +2,25 @@
 const Apartment = require('../models/apartment.model.js');
 
 const getNewApartmentForm = async (req, res) => {
+    const { idApartment } = req.params;
+    let apartment = {}
 
-    res.render('new-apartment.ejs')
+    if (idApartment) {
+        apartment = await Apartment.findById(idApartment);
+        if (!apartment.services) {
+            apartment.services = {}; // Asegura que services siempre sea un objeto
+        }
+    }
+
+    res.render('new-apartment', {
+        apartment
+    })
 }
 
-// 3º paso para poder añadir apartamento después de crear el form y tras crear el endpoint en admin.js routes.
 const postNewApartmentForm = async (req, res) => {
-    const { title, location, description, price, size, mainPhoto, capacity } = req.body
+    const { id, title, location, description, price, size, mainPhoto, capacity } = req.body
 
-    await Apartment.create({
+    const apartment = {
         title,
         location,
         description,
@@ -22,14 +32,36 @@ const postNewApartmentForm = async (req, res) => {
             wifi: req.body.wifi === 'true',
             airConditioner: req.body.airConditioner === 'true'
         }
-    })
+    }
+
+    // Editar apartamento
+    if (id){
+        await Apartment.findByIdAndUpdate(id, apartment);
+        res.send('Apartamento ACTUALIZADO correctamente')
+        return;
+    }
+
+    // Crear nuevo apartamento
+    await Apartment.create(apartment)
     res.send('Apartamento creado')
     
+}
+
+const getEditApartmentForm = async (req, res) => {
+    const { idApartment } = req.params;
+    const apartment = await Apartment.findById(idApartment);
+    const availableServices = Object.keys(apartment.services).filter(service => apartment.services[service])
+
+    res.render('new-apartment', {
+        apartment,
+        availableServices
+    })
 }
 
 // named exports (expotamos varios recursos, lo hacemos como un objeto)
 module.exports = {
     getNewApartmentForm,
-    postNewApartmentForm
+    postNewApartmentForm,
+    getEditApartmentForm
 }
 
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,10 @@ const adminControllers = require('../controllers/admin.js');
 // Endpoint de admin que nos permite mostrar la vista de form para añadir un nuevo apartamento
 router.get('/apartment/new-apartment', adminControllers.getNewApartmentForm);
 
-// Endpoint de admin para poder añadir apartamento desde la vista del form
+// Endpoint de admin que nos permite mostrar la vista de form para editar un nuevo apartamento
+router.get('/apartment/:idApartment/edit', adminControllers.getEditApartmentForm)
+
+// Endpoint de admin para poder añadir o editar apartamento desde la vista del form
 router.post('/apartment/new-apartment', adminControllers.postNewApartmentForm)
 
 module.exports = router;
